558158a7d97f922a2356b22c65d55f9fffd5512b
amanmathankar13 <mathankaraman13@gmail.com>
2025-11-18 20:25:05 +0530

    config changes

--- src/main/java/com/jobportal/job_portal/Configuration/SecurityConfig.java
+++ src/main/java/com/jobportal/job_portal/Configuration/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.jobportal.job_portal.Configuration;
 
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,10 +8,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
 import com.jobportal.job_portal.JWT.JwtAuthenticationEntryPoint;
 import com.jobportal.job_portal.JWT.JwtAuthenticationFilter;
 
@@ -36,30 +30,28 @@ public class SecurityConfig {
         //     return http.build();
         
         http
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(requests -> requests
-                        .requestMatchers("/auth/login", "/users/register", "/users/verify-otp/**", "/users/send-otp/**", "users/changePassword/**").permitAll()
+                        .requestMatchers("/auth/login", "/users/register", "/users/verify-otp/**", "/users/send-otp/**", "/users/changePassword/**").permitAll()
                         .anyRequest()
                         .authenticated())
                 .exceptionHandling(ex -> ex.authenticationEntryPoint(point))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
-        
     }
 
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // Your React app's origin
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Allowed methods
-        configuration.setAllowedHeaders(Arrays.asList("*")); // Allowed headers
-        configuration.setAllowCredentials(true); // Allow credentials (cookies) if needed
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration); // Apply to all endpoints
-        return source;
-    }
+    // @Bean
+    // CorsConfigurationSource corsConfigurationSource() {
+    //     CorsConfiguration configuration = new CorsConfiguration();
+    //     configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // Your React app's origin
+    //     configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Allowed methods
+    //     configuration.setAllowedHeaders(Arrays.asList("*")); // Allowed headers
+    //     configuration.setAllowCredentials(true); // Allow credentials (cookies) if needed
+    //     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    //     source.registerCorsConfiguration("/**", configuration); // Apply to all endpoints
+    //     return source;
+    // }
 
 }
 
